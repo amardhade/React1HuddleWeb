2e8168cbcf51cf9d6d5b5c88c0e49812c9ff92f3
amar.dhade <amardhade@gmail.com>
2021-08-08 19:38:51 +0530

    Selected option highligted

--- src/components/gameQuestions/Questions.js
+++ src/components/gameQuestions/Questions.js
@@ -2,6 +2,7 @@ import React, { userState, useEffect, useContext, useState } from 'react';
 import { Typography, Button } from '@material-ui/core';
 import './GameQuestions.scss';
 import GameContext from '../../context/GameContext';
+import classNames from 'classnames';
 
 const Questions = () => {
 
@@ -9,12 +10,15 @@ const Questions = () => {
     const [questions, setQuestions] = useState([]); // Default empty
     const [question, setQuestion] = useState({}); // Default undefined
     let [questionIndex, setQuestionIndex] = useState(0); // Default 0
+    let [points, setPoints] = useState(0); // Default 0
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    // const [wrongOption, setWrongOption] = useState('');
 
     const getAllQuestions = () => {
         var allQuestions = [];
         game.catrgories.map((category) => {
             const catQuestions = mapCategoryDetailsToQuestion(category);
-            if(catQuestions && catQuestions.length) {
+            if (catQuestions && catQuestions.length) {
                 catQuestions.map((catQuestion) => {
                     allQuestions.push(catQuestion)
                 })
@@ -26,7 +30,6 @@ const Questions = () => {
         if (allQuestions && questionIndex < allQuestions.length - 1) {
             setQuestion(allQuestions[questionIndex]);
         }
-        console.log('Question: ', question);
     }
 
     const mapCategoryDetailsToQuestion = (category) => {
@@ -35,11 +38,22 @@ const Questions = () => {
                 ...question,
                 category_id: category.category_id,
                 category_name: category.category_name,
-                attempted: false
+                attempted: false,
+                selectedAnsIndex: -1,
+                answer_options: mapAnsOptions(question.answer_options)
+
             }
         });
-        console.log('mapCategoryDetailsToQuestion: ', questions);
         return questions;
+    }
+
+    const mapAnsOptions = (ansOptions) => {
+        const ansOptionsAsAObject = ansOptions.map((ansOption) => {
+            return {
+                title: ansOption
+            }
+        });
+        return ansOptionsAsAObject;
     }
 
     const nextQuestion = () => {
@@ -66,10 +80,43 @@ const Questions = () => {
         return questionIndex > 0;
     }
 
-    const questionAttempted = (ansIndex, ansOption) => {
-        console.log(ansIndex, " ", ansOption)
+    const questionAttempted = (selectedIndex, ansOption) => {
+        if (question.attempted) { return; }
+        selectedIndex = selectedIndex + 1; // As selected index starts from 0 where ans_index starts from 1
+        console.log(selectedIndex, ansOption);
+        question.attempted = true;
+        question.selectedAnsIndex = selectedIndex
+        setSelectedIndex(selectedIndex);
+        if (question.answer_index == selectedIndex) { // attempted correctly
+            showAnswers(true, selectedIndex - 1);
+            updatePoints();
 
+        } else { // attempted wrongly
+            showAnswers(false, selectedIndex - 1);
+
+        }
     }
+
+    const showAnswers = (attemptedCorrectly, selectedI) => {
+        if (attemptedCorrectly) {
+            question.answer_options[selectedI] = { ...question.answer_options[selectedI], correct: true }
+        } else {
+            question.answer_options[question.answer_index - 1] = { ...question.answer_options[question.answer_index - 1], correct: true }
+            question.answer_options[selectedI] = { ...question.answer_options[selectedI], incorrect: true }
+        }
+    }
+
+    const updatePoints = () => {
+        points = points + question.points;
+        setPoints(points);
+    }
+
+    const ansOptionClasses = classNames(
+        {
+            'ansOption': true,
+            'correctAns': question.answer_index
+        }
+    );
 
     // Run only once when component renderslike componentDidMount
     useEffect(() => {
@@ -77,32 +124,34 @@ const Questions = () => {
     }, [])
 
     return (
-            <div className="questionWrapper">
-                <div className="questionHeader">
-                    <div className="categoryTitleWrapper">
-                        <Typography variant="subtitle1" className="categoryTitle">{question.category_name}</Typography>
-                        <Typography variant="subtitle1" className="questionCountWrapper">{0}/{0}</Typography>
-                    </div>
-                    <div className="questionSection">
-                        <div className="question">
-                            <Typography variant="subtitle1" className="questionTitle">{question.question_title}</Typography>
-                            {question.image_url != null && question.image_url.length > 0 &&
-                                <img className="questionImage" src={question.image_url}></img>
-                            }
-                        </div>
-                        <div className="answerOptions">
-                            {question.answer_options && question.answer_options.map((ansOption, ansIndex) =>
-                                <div key={ansIndex} className="ansOption" onClick={() => questionAttempted(ansIndex, ansOption)}>
-                                    <span className="ans">{question.answer_options[ansIndex]}</span>
-                                </div>)}
-                        </div>
-                    </div>
+        <div className="questionWrapper">
+            <div className="questionHeader">
+                <div className="categoryTitleWrapper">
+                    <Typography variant="subtitle1" className="categoryTitle">{question.category_name}</Typography>
+                    <Typography variant="subtitle1" className="questionCountWrapper">{0}/{0}</Typography>
                 </div>
-                <div className="questionFooter">
-                    <Button className="previous" disabled={!canGoPrevious()} onClick={() => previousQuestion()}>Previous</Button>
-                    <Button className="next" disabled={!canGoNext()} onClick={() => nextQuestion()}>Next</Button>
+                <div className="questionSection">
+                    <div className="question">
+                        <Typography variant="subtitle1" className="questionTitle">{question.question_title}</Typography>
+                        {question.image_url != null && question.image_url.length > 0 &&
+                            <img className="questionImage" alt="image" src={question.image_url}></img>
+                        }
+                    </div>
+                    <div className="answerOptions">
+                        {question.answer_options && question.answer_options.map((ansOption, index) =>
+                            <div key={index} className={"ansOption " + ((question.attempted && question.answer_index == index + 1) ? 'correctAns' : '')}
+                                style={{ background: (question.attempted && ansOption.incorrect) ? 'red' : '' }}
+                                onClick={() => questionAttempted(index, ansOption)}>
+                                <span className="ans">{question.answer_options[index].title}</span>
+                            </div>)}
+                    </div>
                 </div>
             </div>
+            <div className="questionFooter">
+                <Button className="previous" disabled={!canGoPrevious()} onClick={() => previousQuestion()}>Previous</Button>
+                <Button className="next" disabled={!canGoNext()} onClick={() => nextQuestion()}>Next</Button>
+            </div>
+        </div>
     )
 
 }
